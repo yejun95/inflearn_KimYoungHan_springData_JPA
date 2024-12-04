9e49f84d229eaea6391a8bdf0f3d36465a8b83b7
xodlf100 <xodlf100@gmail.com>
2024-12-04 22:53:42 +0900

    feat data-jpa 폴더 누락으로 인한 추가

---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void testMember()  {
+        // given
+        Member member = new Member("memberA");
+
+        // when
+        Member savedMember = memberRepository.save(member);
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
+    }
+
+}
